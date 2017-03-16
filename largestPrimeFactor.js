fc9ade035df9ffedb225722d41bed5aeab1c2103
Dakota <bazookaharwell@gmail.com>
2017-03-16 09:46:28 -1000

    solution

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,27 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
+  var factors = [];
+  var primeFactors = [];
+  var primeFactor = false;
 
-  return primeNumber;
+
+  for (let i = 2; i < n / 2 - 1; i += 1) {
+    if (n % i === 0) {
+      factors.push(i);
+    }
+  }
+
+  for (let i = 0; i < factors.length; i++) {
+    for (var j = factors[i] - 1; j > 1; j -= 1) {
+      if (factors[i] % j === 0) {
+        primeFactor = false;
+        break;
+      }
+      primeFactor = true;
+    }
+    if (primeFactor) {
+      primeFactors.push(factors[i]);
+    }
+  }
+
+  return primeFactors.pop();
 };
